b4f2e9111216fe8dd57c2fbffc6d469517014ff4
Lars Haugan <lars.haugan@sparebank1.no>
2023-11-20 10:30:27 +0100

    Use latest golang versions
    Signed-off-by: Lars Haugan <lars.haugan@sparebank1.no>

--- build.sh
+++ build.sh
@@ -10,6 +10,7 @@ echo "Building opa-docker-authz version: $VERSION (OPA version: $OPA_VERSION)"
 echo -e "\nBuilding opa-docker-authz ..."
 CGO_ENABLED=0 go build -ldflags \
     "-X github.com/open-policy-agent/opa-docker-authz/version.Version=$VERSION -X github.com/open-policy-agent/opa-docker-authz/version.OPAVersion=$OPA_VERSION" \
+    -buildvcs=false \
     -o opa-docker-authz
 
 echo -e "\n... done!"
