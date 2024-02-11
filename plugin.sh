8f6cd6054f27b5e8e9f75b380f079e9e3f6d2cee
Lars Haugan <lars.haugan@sparebank1.no>
2024-02-11 11:25:01 +0100

    build plugin and images for multiple architectures
    This adds support for building and publishing linux-amd64 and linux-arm64 versions of the plugin.
    We only build new tags with os-arch naming since the docker plugin system does not seem
    to have support for the manifest type: `application/vnd.docker.distribution.manifest.list.v2+json`.
    
    Signed-off-by: Lars Haugan <lars.haugan@sparebank1.no>

--- plugin.sh
+++ plugin.sh
@@ -6,7 +6,7 @@ set -ex
 mkdir ./rootfs
 
 echo "Creating root filesystem for plugin ..."
-docker image build -t rootfsimage .
+docker image build --load -t rootfsimage .
 id=`docker container create rootfsimage true`
 docker container export "$id" | tar -x -C ./rootfs
 
@@ -17,3 +17,29 @@ echo "Cleanup..."
 docker container rm -f "$id" > /dev/null
 docker image rm -f rootfsimage > /dev/null
 rm -rf ./rootfs
+
+
+platforms=("linux/amd64" "linux/arm64")
+for platform in "${platforms[@]}"
+do
+	platform_split=(${platform//\// })
+	GOOS=${platform_split[0]}
+	GOARCH=${platform_split[1]}
+
+	[ -d ./rootfs ] && rm -rf ./rootfs
+	mkdir ./rootfs
+
+	echo "Creating root filesystem for plugin ..."
+	docker buildx build --load --platform ${platform} -t rootfsimage-${GOOS}-${GOARCH} .
+	#docker image build -t rootfsimage .
+	id=`docker container create --platform ${platform} rootfsimage-${GOOS}-${GOARCH} true`
+	docker container export "$id" | tar -x -C ./rootfs
+
+	echo "Creating plugin "${REPO}:${VERSION}-${GOOS}-${GOARCH}" ..."
+	docker plugin create "${REPO}:${VERSION}-${GOOS}-${GOARCH}" .
+
+	echo "Cleanup..."
+	docker container rm -f "$id" > /dev/null
+	docker image rm -f rootfsimage-${GOOS}-${GOARCH} > /dev/null
+	rm -rf ./rootfs
+done
