2490c9d8a301a9dd5f8a2ce643005b560e10ab45
Lars Haugan <lars.haugan@sparebank1.no>
2024-02-11 11:25:01 +0100

    build plugin and images for multiple architectures
    This adds support for building and publishing linux-amd64 and linux-arm64 versions of the plugin.
    
    Signed-off-by: Lars Haugan <lars.haugan@sparebank1.no>

--- plugin.sh
+++ plugin.sh
@@ -1,19 +1,27 @@
 #!/usr/bin/env bash
 
 set -ex
+platforms=("linux/amd64" "linux/arm64")
+for platform in "${platforms[@]}"
+do
+	platform_split=(${platform//\// })
+	GOOS=${platform_split[0]}
+	GOARCH=${platform_split[1]}
 
-[ -d ./rootfs ] && rm -rf ./rootfs
-mkdir ./rootfs
+	[ -d ./rootfs ] && rm -rf ./rootfs
+	mkdir ./rootfs
 
-echo "Creating root filesystem for plugin ..."
-docker image build -t rootfsimage .
-id=`docker container create rootfsimage true`
-docker container export "$id" | tar -x -C ./rootfs
+	echo "Creating root filesystem for plugin ..."
+	docker buildx build --load --platform ${platform} -t rootfsimage-${GOOS}-${GOARCH} .
+	#docker image build -t rootfsimage .
+	id=`docker container create --platform ${platform} rootfsimage-${GOOS}-${GOARCH} true`
+	docker container export "$id" | tar -x -C ./rootfs
 
-echo "Creating plugin "${REPO}:${VERSION}" ..."
-docker plugin create "${REPO}:${VERSION}" .
+	echo "Creating plugin "${REPO}:${VERSION}-${GOOS}-${GOARCH}" ..."
+	docker plugin create "${REPO}:${VERSION}-${GOOS}-${GOARCH}" .
 
-echo "Cleanup..."
-docker container rm -f "$id" > /dev/null
-docker image rm -f rootfsimage > /dev/null
-rm -rf ./rootfs
+	echo "Cleanup..."
+	docker container rm -f "$id" > /dev/null
+	docker image rm -f rootfsimage-${GOOS}-${GOARCH} > /dev/null
+	rm -rf ./rootfs
+done
